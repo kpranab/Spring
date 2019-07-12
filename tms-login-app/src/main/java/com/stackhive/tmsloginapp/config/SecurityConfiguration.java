d0772e2a749e0a56889aeb4cfa01090d3edde36d
Pranab Kumar Sahoo <pranab7sahoo@gmail.com>
2019-07-12 19:16:11 +0530

    Spring Boot : Security login with inmemory usename, password and role

--- tms-login-app/src/main/java/com/stackhive/tmsloginapp/config/SecurityConfiguration.java
+++ tms-login-app/src/main/java/com/stackhive/tmsloginapp/config/SecurityConfiguration.java
@@ -4,6 +4,7 @@
 package com.stackhive.tmsloginapp.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -17,12 +18,19 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.withUser("pranab").password("{noop}test").roles("ADMIN").and()
+			.withUser("pramod").password("{noop}test1").roles("USER");
+	}
+
+	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		
 		http.authorizeRequests()
 //			.antMatchers("*/other*")
 			.anyRequest()
-			.permitAll()
+			.fullyAuthenticated()
 			.and().httpBasic();
 		http.csrf().disable();
 	}
