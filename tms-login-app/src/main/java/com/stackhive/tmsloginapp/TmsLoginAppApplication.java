66aa54565a52c32af36c77d0b635a99a218b6d50
Pranab Kumar Sahoo <pranab7sahoo@gmail.com>
2019-07-12 17:29:33 +0530

    Initial project configuration for login microservice app

---
+++ tms-login-app/src/main/java/com/stackhive/tmsloginapp/TmsLoginAppApplication.java
@@ -0,0 +1,13 @@
+package com.stackhive.tmsloginapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmsLoginAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmsLoginAppApplication.class, args);
+	}
+
+}
---
+++ tms-login-app/src/test/java/com/stackhive/tmsloginapp/TmsLoginAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.stackhive.tmsloginapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TmsLoginAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
