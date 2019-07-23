a4faf6fa008fdcb3d85216015512f964f9fdeb9c
Pranab Kumar Sahoo <pranab7sahoo@gmail.com>
2019-07-23 19:49:40 +0530

    Security configuration file updated

--- tms-login-app/src/main/java/com/stackhive/tmsloginapp/config/SecurityConfiguration.java
+++ tms-login-app/src/main/java/com/stackhive/tmsloginapp/config/SecurityConfiguration.java
@@ -3,25 +3,32 @@
  */
 package com.stackhive.tmsloginapp.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import com.stackhive.tmsloginapp.repository.UsersRepository;
 
 /**
  * @author Pranab Kumar Sahoo
  *
  */
 @EnableWebSecurity
+@EnableJpaRepositories(basePackageClasses = UsersRepository.class)
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private UserDetailsService userDetailsService;
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-			.withUser("pranab").password("{noop}test").roles("ADMIN").and()
-			.withUser("pramod").password("{noop}test1").roles("USER");
+		auth.userDetailsService(userDetailsService);
 	}
 
 	@Override
