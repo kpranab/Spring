5c441c3c5e5ec855756bf02ab3002095273d9603
Pranab Kumar Sahoo <pranab7sahoo@gmail.com>
2019-07-28 12:29:18 +0530

    Organised import

--- tms-login-app/src/main/java/com/stackhive/tmsloginapp/controller/LoginController.java
+++ tms-login-app/src/main/java/com/stackhive/tmsloginapp/controller/LoginController.java
@@ -5,7 +5,6 @@ package com.stackhive.tmsloginapp.controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
