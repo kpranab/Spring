db10a13d1406520a19dcc4e4008f7423371d6cc5
Pranab Kumar Sahoo <pranab7sahoo@gmail.com>
2019-12-08 21:48:31 +0530

    Initial commit for consumer stream app

---
+++ spring-cloud-kafka-stream-consumer/src/main/java/com/spring/cloud/kafka/stream/consumer/springcloudkafkastreamconsumer/SpringCloudKafkaStreamConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.spring.cloud.kafka.stream.consumer.springcloudkafkastreamconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudKafkaStreamConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudKafkaStreamConsumerApplication.class, args);
+	}
+
+}
