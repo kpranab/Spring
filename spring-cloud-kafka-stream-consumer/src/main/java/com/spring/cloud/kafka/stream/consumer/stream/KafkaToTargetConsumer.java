95c442a756d2b82c1e80c2930b556aba59e8ec6e
Pranab Kumar Sahoo <pranab7sahoo@gmail.com>
2020-01-03 23:39:08 +0530

    Consumer mathod changes

--- spring-cloud-kafka-stream-consumer/src/main/java/com/spring/cloud/kafka/stream/consumer/stream/KafkaToTargetConsumer.java
+++ spring-cloud-kafka-stream-consumer/src/main/java/com/spring/cloud/kafka/stream/consumer/stream/KafkaToTargetConsumer.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 
 import com.spring.cloud.kafka.stream.consumer.model.Account;
 import com.spring.cloud.kafka.stream.consumer.model.BillingAccount;
+import com.spring.cloud.kafka.stream.consumer.model.CreditHistory;
 import com.spring.cloud.kafka.stream.consumer.repository.BillingAccountRepository;
 
 import lombok.extern.slf4j.Slf4j;
@@ -22,7 +23,20 @@ public class KafkaToTargetConsumer {
 	public void handleAccount(@Payload Account account) {
 		log.info("Received handleAccount : {}",account);
 		billingAccountRepository.insert(BillingAccount.builder()
-				.account_number(account.getAccount_number()).build());
+				.account_number(account.getAccount_number())
+				.account_status(account.getAccount_status())
+				.account_type(account.getAccount_type())
+				.account_subtype(account.getAccount_subtype())
+				.line_of_business(account.getLine_of_business())
+				.timestamp(account.getTimestamp()).build());
 		
+	}
+
+	@StreamListener(ConsumerTopics.CREDIT_HISTORY)
+	public void handleCreditHistory(@Payload CreditHistory creditHistory) {
+		log.info("Received handleCreditHistory : {}",creditHistory);
+		billingAccountRepository.insert(BillingAccount.builder()
+				.account_number(creditHistory.getAccount_number())
+				.credit_class(creditHistory.getCredit_class()).build());
 	}
 }
