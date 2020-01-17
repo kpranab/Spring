10d22be618c6a45ce99aab651ea10565f3d36d56
Pranab Kumar Sahoo <pranab7sahoo@gmail.com>
2020-01-17 23:50:21 +0530

    updated code

--- Spring-Netflix-Eureka/account-client/src/main/java/com/spring/client/account/AccountController.java
+++ Spring-Netflix-Eureka/account-client/src/main/java/com/spring/client/account/AccountController.java
@@ -1,20 +1,12 @@
 package com.spring.client.account;
 
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestClientException;
-import org.springframework.web.client.RestTemplate;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
--- Spring-Netflix-Eureka/account-client/src/main/java/com/spring/client/account/RemoteAccountRepository.java
+++ Spring-Netflix-Eureka/account-client/src/main/java/com/spring/client/account/RemoteAccountRepository.java
@@ -17,48 +17,42 @@ import org.springframework.web.client.RestTemplate;
  *
  */
 public class RemoteAccountRepository implements AccountRepository {
-	
+
 	@Autowired
 	protected RestTemplate restTemplate;
-	
+
 	protected String serviceUrl;
-	
+
 	public RemoteAccountRepository(String serviceUrl) {
-		this.serviceUrl = serviceUrl.startsWith("http") ? serviceUrl
-				: "http://" + serviceUrl;
+		this.serviceUrl = serviceUrl.startsWith("http") ? serviceUrl : "http://" + serviceUrl;
 	}
-	
+
 	@Override
 	public List<Account> getAllAccounts() {
-		Account[] accounts = restTemplate.getForObject(serviceUrl+"/accounts", Account[].class);
+		Account[] accounts = restTemplate.getForObject(serviceUrl + "/accounts", Account[].class);
 		return Arrays.asList(accounts);
 	}
 
 	@Override
 	public Account getAccount(Long number) {
-		return restTemplate.getForObject(serviceUrl + "/accounts/{id}",
-				Account.class, number);
+		return restTemplate.getForObject(serviceUrl + "/accounts/{id}", Account.class, number);
 	}
 
 	@Override
 	public Employee getEmployee() {
-		String baseUrl = serviceUrl+"/employee";
 		ResponseEntity<Employee> empResponse = null;
-		try{
-		empResponse = restTemplate.exchange(baseUrl,
-				HttpMethod.GET, getHeaders(),Employee.class);
-		}catch (Exception ex)
-		{
+		try {
+			empResponse = restTemplate.exchange(serviceUrl + "/employee", HttpMethod.GET, getHeaders(), Employee.class);
+		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
 		return empResponse.getBody();
 	}
-	
+
 	private static HttpEntity<?> getHeaders() throws IOException {
 		HttpHeaders headers = new HttpHeaders();
 		headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
 		return new HttpEntity<>(headers);
 	}
-	
 
 }
