9a0583048b810a518dc9eb45436c97a9ff3f8923
Pranab Kumar Sahoo <pranab7sahoo@gmail.com>
2020-02-21 21:34:03 +0530

    Removed WebClient implementation for communicating between microservices

--- Spring-Microservices/movie-catalog/src/main/java/com/movie/catalog/moviecatalog/MovieCatalogApplication.java
+++ Spring-Microservices/movie-catalog/src/main/java/com/movie/catalog/moviecatalog/MovieCatalogApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class MovieCatalogApplication {
@@ -12,11 +11,6 @@ public class MovieCatalogApplication {
     @Bean
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
-    }
-
-    @Bean
-    public WebClient.Builder getWebClientBuilder() {
-        return WebClient.builder();
     }
 
     public static void main(String[] args) {
