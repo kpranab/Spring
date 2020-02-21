04345a8313b5f57fef9bafd381ff98e2cf777723
Pranab Kumar Sahoo <pranab7sahoo@gmail.com>
2020-02-21 21:24:52 +0530

    Spring boot microservices implementation

---
+++ Spring-Microservices/movie-catalog/src/main/java/com/movie/catalog/moviecatalog/models/UserRating.java
@@ -0,0 +1,16 @@
+package com.movie.catalog.moviecatalog.models;
+
+import java.util.List;
+
+public class UserRating {
+
+    private List<Rating> userRating;
+
+    public List<Rating> getUserRating() {
+        return userRating;
+    }
+
+    public void setUserRating(List<Rating> userRating) {
+        this.userRating = userRating;
+    }
+}
---
+++ Spring-Microservices/movie-rating/src/main/java/com/movie/rating/movierating/controller/MovieRatingController.java
@@ -0,0 +1,28 @@
+package com.movie.rating.movierating.controller;
+
+import com.movie.rating.movierating.models.Rating;
+import com.movie.rating.movierating.models.UserRating;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/rating")
+public class MovieRatingController {
+
+    @RequestMapping("/{movieId}")
+    public Rating getRating(@PathVariable("movieId") String movieId) {
+        return new Rating(movieId, 4);
+    }
+
+    @RequestMapping("/user/{userId}")
+    public UserRating getUserRating(@PathVariable("userId") String userId) {
+        List<Rating> ratings = Arrays.asList(new Rating("Pranab", 4), new Rating("Pramod", 5));
+        UserRating userRating = new UserRating();
+        userRating.setUserRating(ratings);
+        return userRating;
+    }
+}
