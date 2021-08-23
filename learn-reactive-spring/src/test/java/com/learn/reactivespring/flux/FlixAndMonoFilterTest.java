f13af22f01c9918272ea61eae61df4579217b303
Pranab Kumar Sahoo <pranab7sahoo@gmail.com>
2021-08-23 08:34:48 +0530

    Flux transform parallel

--- learn-reactive-spring/src/test/java/com/learn/reactivespring/flux/FlixAndMonoFilterTest.java
+++ learn-reactive-spring/src/test/java/com/learn/reactivespring/flux/FlixAndMonoFilterTest.java
@@ -7,6 +7,8 @@ import reactor.test.StepVerifier;
 import java.util.Arrays;
 import java.util.List;
 
+import static reactor.core.scheduler.Schedulers.parallel;
+
 public class FlixAndMonoFilterTest {
 
     List<String> names = Arrays.asList("hi", "hello","bye","tata");
@@ -79,4 +81,57 @@ public class FlixAndMonoFilterTest {
                 .expectNext("HELLO")
                 .verifyComplete();
     }
+
+    @Test
+    public void transformUsingFlatMap(){
+        Flux<String> stringFlux = Flux.fromIterable(Arrays.asList("A","B","C","D","E","F"))
+                .flatMap(s -> {
+                    return Flux.fromIterable(convertToList(s));
+                })
+                .log();
+
+        StepVerifier.create(stringFlux)
+                .expectNextCount(12)
+                .verifyComplete();
+    }
+
+    private List<String> convertToList(String s) {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return Arrays.asList(s,"newValue");
+    }
+
+    @Test
+    public void transformUsingFlatMap_Parallel(){
+        Flux<String> stringFlux = Flux.fromIterable(Arrays.asList("A","B","C","D","E","F"))
+                .window(2)
+                .flatMap((s) ->
+                        s.map(this::convertToList).subscribeOn(parallel()))
+                .flatMap(s -> Flux.fromIterable(s))
+                .log();
+
+        StepVerifier.create(stringFlux)
+                .expectNextCount(12)
+                .verifyComplete();
+    }
+
+    @Test
+    public void transformUsingFlatMap_Parallel_MaintainOrder(){
+        Flux<String> stringFlux = Flux.fromIterable(Arrays.asList("A","B","C","D","E","F"))
+                .window(2)
+                /*.concatMap((s) ->
+                        s.map(this::convertToList).subscribeOn(parallel()))*/
+                .flatMapSequential((s) ->
+                        s.map(this::convertToList).subscribeOn(parallel()))
+                .flatMap(s -> Flux.fromIterable(s))
+                .log();
+
+        StepVerifier.create(stringFlux)
+                .expectNextCount(12)
+                .verifyComplete();
+    }
+
 }
